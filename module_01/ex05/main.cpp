c84e0820070da351b5f92eb4e3549a012dbe85e8
DolMalin <dolmalinn@gmail.com>
2022-12-01 11:20:17 +0100

    refactor(01-05): use main args to fill harl

--- module_01/ex05/main.cpp
+++ module_01/ex05/main.cpp
@@ -1,22 +1,14 @@
 #include "Harl.hpp"
 
-int main(void)
+int main(int ac, char **av)
 {
 	Harl harl;
 
-	std::cout << std::endl << "DEBUG" << std::endl;
-	harl.complain("DEBUG");
-
-	std::cout << std::endl << "INFO" << std::endl;
-	harl.complain("INFO");
-
-	std::cout << std::endl << "WARNING" << std::endl;
-	harl.complain("WARNING");
-
-	std::cout << std::endl << "ERROR" << std::endl;
-	harl.complain("ERROR");
-
-	std::cout << std::endl << "_blank_" << std::endl;
-	harl.complain("");
+	if (ac != 2)
+	{
+		std::cout << "Invalid arguments" << std::endl;
+		return 1;
+	}
+	harl.complain(av[1]);
 	return 0;
 }
