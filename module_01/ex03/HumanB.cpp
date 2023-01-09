ef9c1687bdb95d87f77c47ac26479ab28f1f6a61
Paul Dal molin <pdal-mol@z4r2p5.42lyon.fr>
2023-01-09 10:55:12 +0100

    fix(01-03): segfault due to weapon unitiliazed to NULL

--- module_01/ex03/HumanB.cpp
+++ module_01/ex03/HumanB.cpp
@@ -1,10 +1,14 @@
 #include "HumanB.hpp"
 
-HumanB::HumanB(std::string name): _name(name) {}
+HumanB::HumanB(std::string name): _name(name), _weapon(NULL){}
 
 void HumanB::attack()
 {
-	std::cout << _name << " attacks with their " << _weapon->getType() << std::endl;
+	if (!_weapon)
+		std::cout << _name << " attacks with their fist " << std::endl;
+	else
+		std::cout << _name << " attacks with their " << _weapon->getType() << std::endl;
+
 }
 
 void HumanB::setWeapon(Weapon &weapon)
