e4ce3de7e0998c5fcc37f8b1a6ff4c7b35df13e3
Paul Dal molin <pdal-mol@z4r2p3.42lyon.fr>
2023-01-24 15:50:45 +0100

    feat(08-02): exercise done

---
+++ module_08/ex02/MutantStack.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <stack>
+#include <iostream>
+
+template<typename T>
+class MutantStack : public std::stack<T>
+{
+public:
+	MutantStack() {}
+	MutantStack(const MutantStack<T>& src) { *this = src; }
+	MutantStack<T>& operator=(const MutantStack<T>& rhs) 
+	{
+		this->c = rhs.c;
+		return *this;
+	}
+	~MutantStack() {}
+
+	typedef typename std::stack<T>::container_type::iterator iterator;
+    typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+
+	iterator begin() { return this->c.begin(); }
+	iterator end() { return this->c.end(); }
+
+    const_iterator begin() const { return this->c.begin(); }
+	const_iterator end() const { return this->c.end(); }
+};
